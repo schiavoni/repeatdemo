b36492fbf4f723e9c75595fef3239a9e18562ad4
Rafael Schiavoni <schiavoni.rt@gmail.com>
2021-11-19 11:48:06 -0300

    LTV starting

--- shopify_tst.rb
+++ shopify_tst.rb
@@ -7,9 +7,51 @@ ShopifyAPI::Base.site = "https://#{ENV['SHOPIFY_API_KEY']}:#{ENV['SHOPIFY_PASSWO
 ShopifyAPI::Base.api_version = '2020-10'
 
 class LifeTimeValue
-    attr_reader :total_time, :total_spend, :total_clients
+    attr_reader :total_time, :total_spent, :total_clients, :total_orders, :total_repeatable_orders, :avg_spent_by_client, :avg_spent_by_order, :avg_time, :avg_time_between_orders, :avg_orders_count_by_time
+    @allClients = Array.new
     
-    
+    def initialize(allClients)
+        @allClients = allClients
+        @total_clients = @allClients.length()
+        @total_time = 0
+        @total_spent = 0
+        @total_orders = 0
+        @total_repeatable_orders = 0
+        @avg_spent_by_client = 0
+        @avg_spent_by_order = 0
+        @avg_time = 0
+        @avg_time_between_orders = 0
+        @avg_orders_count_by_time = 0
+
+        puts "total_clients: #{total_clients}"
+    end
+
+    def calculate()
+        for client in @allClients do
+            @total_time = @total_time + client.total_time
+            @total_spent = @total_spent + client.total_spent
+            @total_orders = @total_orders + client.orders_count
+
+            if client.orders_count > 1
+                @total_repeatable_orders = @total_repeatable_orders + client.orders_count
+            end
+        end
+
+        @avg_spent_by_client = @total_spent / @total_clients
+        @avg_spent_by_order = @total_spent / @total_orders
+        @avg_time = @total_time / @total_repeatable_orders
+    end
+
+    def showAll()
+        puts "total_time: #{total_time}
+total_spent: #{total_spent}
+total_clients: #{total_clients}
+total_orders: #{total_orders} 
+avg_spent_by_client: #{avg_spent_by_client}
+avg_spent_by_order: #{avg_spent_by_order} 
+avg_time: #{avg_time}
+avg_orders_count_by_time: #{avg_orders_count_by_time}"
+    end    
 end
 
 #interfaces use raise NotImplementedError, "Implement this method in a child class"
@@ -44,18 +86,22 @@ end
 
 class Client_totals
     @@allClients = Array.new
-    attr_accessor :id, :first_order_at, :last_order_at, :total_spent
 
-    def self.all_instances
+    attr_accessor :id, :first_order_at, :last_order_at, :total_spent, :total_time, :orders_count
+
+    def self.all_clients
         @@allClients
     end
 
     def initialize(order)
         @id = order.customer.id
+        @orders_count = order.customer.orders_count.to_i
         @first_order_at = order.created_at
         @last_order_at = order.created_at
-        @total_spent = order.customer.total_spent
+        @total_spent = order.customer.total_spent.to_i
+        @total_time = 0
 
+        #by clean code, no logic should be here. Iam sorry =/
         addOrChange()
     end
 
@@ -63,10 +109,12 @@ class Client_totals
         foundIndex = @@allClients.index { |client| client.id == @id }
         if foundIndex == nil
             @@allClients << self
+            updateTotalTime(@@allClients.length() - 1)
             #puts "#{id} added"
         else
             #puts "#{foundIndex} found"
             updateDates(foundIndex)
+            updateTotalTime(foundIndex)
         end
     end
 
@@ -83,6 +131,10 @@ class Client_totals
         end
         #puts "-----"
     end
+
+    def updateTotalTime(foundIndex)
+        @@allClients[foundIndex].total_time = Time.iso8601(@@allClients[foundIndex].last_order_at) - Time.iso8601(@@allClients[foundIndex].first_order_at)
+    end
 end
 
 class Dashboard
@@ -91,6 +143,7 @@ class Dashboard
     def initialize()
         @orders = ShopifyAPI::Order.find(:all, params: { limit: 25 })
         #TODO should only return the fields being used1
+        countPlacedOrders()
     end
 
     def countPlacedOrders
@@ -104,6 +157,10 @@ class Dashboard
             @orders = @orders.fetch_next_page
             countOrders(@orders)
         end
+
+        ltv = LifeTimeValue.new(Client_totals.all_clients)
+        ltv.calculate()
+        ltv.showAll()
     end
 
     def countOrders(orders)
